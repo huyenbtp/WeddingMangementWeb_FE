62a422e15c3f1f8258db3c5c734cbfdfdac12d88
nguyenhoangtuan123 <154316041+nguyenhoangtuan123@users.noreply.github.com>
2025-06-16 19:08:52 +0700

    thêm login

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -6,13 +6,12 @@ import Food from "../pages/Food/Food";
 import Service from "../pages/Service/Service";
 import Report from "../pages/Report/Report";
 import Settings from "../pages/Settings/Settings.tsx";
-import Homepage from "../pages/Homepage/Homepage";
-import BlankPage from "../pages/BlankPage/BlankPage";
+import Login from "../pages/Login/Login.tsx"
+import Register from "../pages/Login/Register.tsx"
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Homepage />} />
       <Route path="/sanh-tiec" element={<Hall />} />
       <Route path="/tiec-cuoi" element={<Party />} />
       <Route path="/lich-su-kien/:view" element={<Schedule />} />
@@ -20,8 +19,9 @@ export default function AppRoutes() {
       <Route path="/dich-vu" element={<Service />} />
       <Route path="/bao-cao" element={<Report />} />
       <Route path="/cai-dat" element={<Settings />} />
-      <Route path="/blank-page" element={<BlankPage />} />
-      <Route path="*" element={<Homepage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<Hall />} />
     </Routes>
   );
 }
